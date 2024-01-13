5da9498d00f4111ea1df09d12fe845d8593156fe
chafaatoumk <chafaatoumk1998@gmail.com>
2024-01-13 20:52:14 +0100

    Dynamic map loading

--- dataViz-project/src/app/components/conflict-stats/conflict-stats.component.ts
+++ dataViz-project/src/app/components/conflict-stats/conflict-stats.component.ts
@@ -1,26 +1,67 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 
 import * as d3 from 'd3';
-import { geoMercator, geoPath } from 'd3';
+import { geoMercator, geoPath, ZoomBehavior, Selection, BaseType } from 'd3';
 
 @Component({
   selector: 'app-conflict-stats',
   templateUrl: './conflict-stats.component.html',
   styleUrls: ['./conflict-stats.component.scss']
 })
-export class ConflictStatsComponent {
+export class ConflictStatsComponent implements OnInit {
   data: any;
+  selectedCountry: string = 'Russia';
+  countries: { value: string; label: string; path:string }[] = [
+    { value: 'Russia', label: 'Russia', path: '../../../assets/data/russia_geojson/Russia.geojson'},
+    { value: 'Ukraine', label: 'Ukraine', path: '../../../assets/data/ukraine_geojson/UA_FULL_Ukraine.geojson'}
+    // Add more countries as needed
+  ];
 
-  constructor() {
+  constructor() {}
 
+  /**
+   * Load data for the default country on app loading
+   */
+  ngOnInit() {
+    this.loadData();
   }
-  ngAfterViewInit() {
-    // Load data
-    d3.json('../../../assets/data/russia_geojson/Russia.geojson').then(
+
+  /**
+   * Load inial data
+   */
+  AfterViewInit() {
+    // Load initial data
+    this.loadData();
+  }
+
+  /**
+   * Load data for the selected country
+   */
+  onCountryChange() {
+    this.loadData();
+  }
+
+  getCountryPath(value: string): string {
+    const country = this.countries.find(country => country.value === value);
+    return country ? country.path : '';
+  }
+
+  /**
+   * Load data
+   */
+  loadData () {
+    const countryPath = this.getCountryPath(this.selectedCountry);
+    // console.log('Country path : ', countryPath);
+
+    d3.json(countryPath).then(
       (jsonData) => {
         this.data = jsonData;
         console.log('Data loaded :', jsonData);
 
+        // Clear previous map
+        d3.select('#map-container').selectAll('*').remove();
+
+        // draw selected country map
         this.drawMap(this.data);
       },
       (erreur) => {
@@ -42,51 +83,67 @@ export class ConflictStatsComponent {
 
     // Define projection
     const projection = geoMercator()
-      .center([100, 71])  // Adjust the center to focus on the region
+      .center([105, 71])  // Adjust the center to focus on the region
       .translate([width / 2, height / 2])
       .scale(200);
+
     const path = geoPath().projection(projection);
-    const svg = d3.select('#map-container').append('svg')
+
+    const svg = d3.select('#map-container')
+      .append('svg')
       .attr('width', width)
       .attr('height', height);
 
     // Create a tooltip div
     const tooltip = d3
-      .select("#conflict-stats-container")
+      .select("body")
       .append("div")
       .attr("class", "hidden tooltip");
 
-    // Draw map
-    svg
-      .selectAll('path')
+    // Create a group for the map features
+    const mapGroup = svg.append('g');
+
+    // Draw map features
+    mapGroup
+      .selectAll(".region")
       .data(jsonData.features)
-      .enter()
-      .append("path")
-      .attr("d", (d: any) => path(d))
-      .attr("class", "region")
-      .attr('fill', 'steelblue')
+      .join('path')
+      .attr('class', 'region')
+      .attr('d', (d: any) => path(d))
+      .style('fill', 'steelblue')
       .attr('stroke', 'black')
-      .on("mousemove", (event: any, d: any) => {
+      .on('mousemove', (event: any, d: any) => {
         const mousePosition = [event.x, event.y];
         // Show tooltip on mousemove
         tooltip
-          .classed("hidden", false)
+          .classed('hidden', false)
           .attr(
-            "style",
-            "left:" +
-              (mousePosition[0]) +
-              "px; top:" +
-              (mousePosition[1]) +
-              "px"
+            'style',
+            'left:' +
+              (mousePosition[0] + 15) +
+              'px; top:' +
+              (mousePosition[1] - 35) +
+              'px'
           )
-          .html(d.properties.name_latin);
+          .html(() => {
+            return this.selectedCountry === 'Russia'
+              ? d.properties.name_latin : d.properties["name:fr"];
+          });
       })
-      .on("mouseout", () => {
+      .on('mouseout', () => {
         // Hide tooltip on mouseout
-        tooltip.classed("hidden", true);
+        tooltip.classed('hidden', true);
       });
 
-    console.log('Map created successfully !');
-  }
+    // Enable zooming
+    const zoom: ZoomBehavior<Element, unknown> = d3.zoom()
+      .scaleExtent([1, 8])  // Adjust the scale extent as needed
+      .on('zoom', (event) => {
+        mapGroup.attr('transform', event.transform);
+      });
 
+    svg.call(zoom as any);
+
+    console.log('Map created successfully!');
+  }
 }
